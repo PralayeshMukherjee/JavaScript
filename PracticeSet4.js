3c3c497c8f6cce7c85bafaa9bfeda60e40bc685c
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2024-12-01 21:16:31 +0530

    add a file to solve problem set 4

---
+++ PracticeSet4.js
@@ -0,0 +1,5 @@
+// what will the following print in js Console.log("har\"'.lenght)
+// Expbre the includes, start with & end with functions of a string
+// write a program to convert a given string to lowercase
+// extract the amount out of this string " Please give Rs 1000"
+// try to change the 4th character of a given string were you able to do it
