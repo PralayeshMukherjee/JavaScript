c9187ad9f7bf459c220aa9f166cdfb66e3fd7ef5
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2024-12-22 15:35:17 +0530

    upload window concept of js

---
+++ window.js
@@ -0,0 +1,7 @@
+// window is the main object in the js. inside the window object we have Document Object Model(DOM), Browser Object Model(BOM) and core JavaScript
+console.log(window);
+window.console.log("Raj");//we can print anything in the console by this method also
+//What is DOM?
+// the whole html page is our document and js convert the whole html page into a JS object and we named it as document object
+console.log(document);//it print the whole html page int the form of object
+console.log(document.body);//it print the document body 
