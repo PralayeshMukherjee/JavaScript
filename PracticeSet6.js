78e69266dc1ef3488c5c2c541b083c9553cdc41a
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2024-12-23 23:49:10 +0530

    Enter all the questions and solved the first question

---
+++ PracticeSet6.js
@@ -0,0 +1,15 @@
+// Q1) Write a program using prompt function to take input of age as a value from the user and use alert to tell him if he can drive.
+let age = prompt("Enter your age: ");
+age = Number.parseInt(age);
+if(age>=18){
+    alert("You can drive");
+}else{
+    alert("You cann't drive");
+}
+
+
+
+// Q2) In Q1 use confirm to ask the user if he wants to see the prompt again
+// Q3) In the previous question use console.error to log the error if the age entered is negative
+// Q4) Write a program to change the url to google.com(redirection) if user enters a number greater than 4
+// Q5) Change the background of the page to yellow red or any other color based on user input throught prompt
