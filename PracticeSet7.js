13ccec0d10f6162f0d9e24c713cc037e58994298
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2024-12-30 11:35:08 +0530

    add the html and js file for question on dom and some answers

---
+++ PracticeSet7.js
@@ -0,0 +1,13 @@
+// Q1) Create a navbar and change the color of its first element to red
+
+// let t = document.body.firstElementChild.firstElementChild.firstElementChild;
+// console.log(t);
+// t.style.color = "red";
+
+// Q2) Create a table without tbody. Now use "View page source" button to check whether it has a tbody or not
+
+
+// Q3) Create an element with 3 children. Now change the color of fist and last element to green.
+// Q4) Write a js code to change background of all <li> tags to cyan.
+// Q5)Which of the following is used to look for the for thest ancestor that matches a given css selector
+// a) matches b)closet c) contains d) none of these
