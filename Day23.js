411f99077b4d9ed32755fc716d623e7bf6801790
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-01-23 20:00:57 +0530

    upload fetch api codes

---
+++ Day23.js
@@ -0,0 +1,26 @@
+let p = fetch("https://api.open-meteo.com/v1/forecast?latitude=28.6139&longitude=77.2090&hourly=temperature_2m");
+p.then((value1)=>{
+    console.log(value1);
+    console.log(value1.status);
+    return value1.json();
+}).then((value2)=>{
+    console.log(value2);
+})
+
+let options = {
+    method: "POST",
+    headers: {
+        "Content-type":"application/json"
+    },
+    body: JSON.stringify({
+        title: 'foo',
+        body: 'bar',
+        userId: 1,
+    }),
+}
+let p1 = fetch('https://jsonplaceholder.typicode.com/posts',options);
+p1.then((value)=>{
+    return value.json();
+}).then((value)=>{
+    console.log(value);
+})
