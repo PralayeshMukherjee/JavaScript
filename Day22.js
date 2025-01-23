4bf9241bc729ec96d133ce11bf93be1b7c737250
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-01-23 12:02:52 +0530

    add an example that how to throw a coustom error

--- Day22.js
+++ Day22.js
@@ -1,19 +1,39 @@
-setTimeout(()=>{
-    console.log("searching wifi...");
-},1000)
-let p1 = new Promise((resolve,reject)=>{
-    setTimeout(()=>{
-        console.log("fetching wifi...");
-        resolve(54);
-    },2000);
-})
-p1.then(()=>{
-    try{
-        console.log(raj);
-    }catch(error){
-        console.log(error);
+// setTimeout(()=>{
+//     console.log("searching wifi...");
+// },1000)
+// let p1 = new Promise((resolve,reject)=>{
+//     setTimeout(()=>{
+//         console.log("fetching wifi...");
+//         resolve(54);
+//     },2000);
+// })
+// p1.then(()=>{
+//     try{
+//         console.log(raj);
+//     }catch(error){
+//         console.log(error);
+//     }
+// })
+// setTimeout(()=>{
+//     console.log("wifi connected successfully");
+// },3000);
+
+// try{
+//     console.log(raj);
+//     throw new ReferenceErrorError("Raj is not good");
+// }catch(error){
+//     console.log(error.name);
+//     console.log(error.message);
+//     console.log(error.stack);
+// }
+
+try{  
+    let age = prompt("Enter your age");
+    age = Number.parseInt(age);
+    if(age>180){
+        throw new Error("it is not true");
     }
-})
-setTimeout(()=>{
-    console.log("wifi connected successfully");
-},3000);
+}catch(error){
+    console.log(error.message);
+}
+console.log("the script is still running");
