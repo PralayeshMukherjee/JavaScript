dcb2c2325fa5c419cfcb86d04f3775e862e4607e
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-01-23 12:51:18 +0530

    upload all exception handaling things

--- Day22.js
+++ Day22.js
@@ -1,31 +1,31 @@
-// setTimeout(()=>{
-//     console.log("searching wifi...");
-// },1000)
-// let p1 = new Promise((resolve,reject)=>{
-//     setTimeout(()=>{
-//         console.log("fetching wifi...");
-//         resolve(54);
-//     },2000);
-// })
-// p1.then(()=>{
-//     try{
-//         console.log(raj);
-//     }catch(error){
-//         console.log(error);
-//     }
-// })
-// setTimeout(()=>{
-//     console.log("wifi connected successfully");
-// },3000);
+setTimeout(()=>{
+    console.log("searching wifi...");
+},1000)
+let p1 = new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        console.log("fetching wifi...");
+        resolve(54);
+    },2000);
+})
+p1.then(()=>{
+    try{
+        console.log(raj);
+    }catch(error){
+        console.log(error);
+    }
+})
+setTimeout(()=>{
+    console.log("wifi connected successfully");
+},3000);
 
-// try{
-//     console.log(raj);
-//     throw new ReferenceErrorError("Raj is not good");
-// }catch(error){
-//     console.log(error.name);
-//     console.log(error.message);
-//     console.log(error.stack);
-// }
+try{
+    console.log(raj);
+    throw new ReferenceErrorError("Raj is not good");
+}catch(error){
+    console.log(error.name);
+    console.log(error.message);
+    console.log(error.stack);
+}
 
 try{  
     let age = prompt("Enter your age");
@@ -37,3 +37,13 @@ try{
     console.log(error.message);
 }
 console.log("the script is still running");
+
+try{
+    let a = 0;
+    console.log(b);
+    console.log("program run successfully");
+}catch(error){
+    console.log(error.message);
+}finally{
+    console.log("finally block executed");
+}
